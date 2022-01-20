c8db2952e5ead2d5fd714f643d297518a00883ee
Eric Lee <ericlovesmath@gmail.com>
2022-01-19 23:35:09 -0800

    Advent of Code 2021, Start

---
+++ aoc-2021/src/day01.rs
@@ -0,0 +1,22 @@
+use crate::utils::read_data;
+
+pub fn run() {
+    println!("{}", day1a());
+    println!("{}", day1b());
+}
+pub fn day1a() -> String {
+    "PLACEHOLDER 1A".to_string()
+}
+
+pub fn day1b() -> String{
+    "PLACEHOLDER 1B".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn part_1a() {
+        assert_eq!(super::day1a(), "");
+    }
+}
