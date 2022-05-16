83c9354645bf99240a10f3d71d4c617b8b973e88
Eric Lee <ericlovesmath@gmail.com>
2022-05-15 19:11:45 -0700

    grep clone

--- syntax/src/rect.rs
+++ syntax/src/rect.rs
@@ -12,7 +12,7 @@ impl Rectangle {
     pub fn perimeter(&self) -> u32 {
         2 * (self.width + self.height)
     }
-    
+
     pub fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
@@ -22,5 +22,39 @@ impl Rectangle {
             width: size,
             height: size,
         }
+    }
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+impl Summary for Rectangle {
+    fn summarize(&self) -> String {
+        format!("Rect is size {} by {}", self.width, self.height)
+    }
+}
+
+// if Generic can implement Summary
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn lifetimes() {
+
+    let string1 = String::from("abcd");
+    let string2 = String::from("xyz");
+
+    let result: &str = longest(string1.as_str(), string2.as_str());
+    println!("The longest string is {}", result);
+}
+
+/// Compiler doesn't know if x or y have the same lifetime
+/// Explicit lifetime notation for the var with shorter lifetime
+pub fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
